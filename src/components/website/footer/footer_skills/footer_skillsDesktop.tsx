6c5555bbce05edca1f142d207e41faf9d71cb7ab
VENKATESH A BETAGERI <venkatesh.b2@happiestminds.com>
2025-04-30 13:03:07 +0530

    [CU-86cyqmtmt] created footer

---
+++ src/components/website/footer/footer_skills/footer_skillsDesktop.tsx
@@ -0,0 +1,33 @@
+// import React from "react";
+import {skills} from '../../../../constants/footer'
+export default function DesktopAccordion() {
+    
+    return (
+      <div className="bg-[#1d1e27] border-t border-[#d1d7dc] pt-6 px-6">
+        <div className="text-[clamp(1rem,1.02rem+0.2vw,1.125rem)] text-[#fff] font-bold mt-4 mb-6">
+          Explore top skills and certifications
+        </div>
+  
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-4 gap-x-8 pb-6">
+          {skills.map((skill, index) => (
+            <div key={index} className="flex flex-col">
+              <h3 className="text-fluid-heading text-[#fff] text-base font-semibold my-4">{skill.title}</h3>
+              <ul className="flex flex-col">
+                {skill.features.map((feature, idx) => (
+                  <li key={idx}>
+                    <a
+                      href={feature.link}
+                      className="text-[#fff] text-fluid-body2 hover:underline pb-1"
+                    >
+                      {feature.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
---
+++ src/components/website/footer/footer_udemybusiness/footer_udemybusinessMobile.tsx
@@ -0,0 +1,51 @@
+//  import React from 'react';
+
+const MobileFooterSection = () => {
+    return (
+      <div className="bg-[#1d1e27] border-t border-b border-[#d1d7dc] py-3 px-6 flex flex-col">
+        <div className="text-[clamp(1rem,0.9rem+0.2vw,1.125rem)] text-fluid-heading font-bold font-weight-inherit text-[#fff] leading-6 pt-[12px] pb-[12px] pr-[24px]">
+          <span>Top companies choose{' '}
+              <a
+              href="/udemy-business/?locale=en_US&path=request-demo-in-mx%2F&ref=footer-ad"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[#c0c4fc]  hover:underline"
+              >
+              Udemy Business
+              </a>{' '}
+              to build in-demand career skills.
+          </span>
+          
+        </div>
+        <div className="flex flex-wrap gap-4">
+          <img
+            src="https://s.udemycdn.com/partner-logos/v4/nasdaq-light.svg"
+            alt="Nasdaq"
+            className="h-[68px] w-[120px] pt-[12px] pb-[12px] pr-[24px]"
+            loading="lazy"
+          />
+          <img
+            src="https://s.udemycdn.com/partner-logos/v4/volkswagen-light.svg"
+            alt="Volkswagen"
+            className="h-[68px] w-[68px] pt-[12px] pb-[12px] pr-[24px]"
+            loading="lazy"
+          />
+          <img
+            src="https://s.udemycdn.com/partner-logos/v4/netapp-light.svg"
+            alt="NetApp"
+            className="h-[68px] w-[120px] pt-[12px] pb-[12px] pr-[24px]"
+            loading="lazy"
+          />
+          <img
+            src="https://s.udemycdn.com/partner-logos/v4/eventbrite-light.svg"
+            alt="Eventbrite"
+            className="h-[68px] w-[120px] pt-[12px] pb-[12px]"
+            loading="lazy"
+          />
+        </div>
+      </div>
+    );
+  };
+  
+  export default MobileFooterSection;
+  
